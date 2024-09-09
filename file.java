62408b4af6bc8ea81e7ef80e2cc2b7f9349943a0
root <root@GIT.fm15pel3r0weblqchzxd2bbs0h.rx.internal.cloudapp.net>
2024-09-09 10:51:00 +0000

    This is my second commit

--- file.java
+++ file.java
@@ -34,4 +34,14 @@ System.out.println();
 //function calling  
 printPattern(n);  
 }  
+} 
+public static void main(String args[])  
+{  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter the value of n: ");  
+int n = sc.nextInt();  
+System.out.println();  
+//function calling  
+printPattern(n);  
+}  
 }  
